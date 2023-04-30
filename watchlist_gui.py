de0286dffd6fc3db7be9a04baaca4cd3f88b79fb
HLIN9 <han.lin1@ucalgary.ca>
2023-04-30 15:58:12 -0600

    Working on GUI layout

--- watchlist_gui.py
+++ watchlist_gui.py
@@ -4,5 +4,30 @@ import tkinter.ttk as ttk
 from web_scraping import get_rating
 
 class Watchlist_GUI(ttk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.grid()
+
+        #TODO: Gets the new watchlist
+        self.watchlist = tk.StringVar()
+        self.watchlist.set('Watchlist')
+
+        #TODO: Make the needed widgets
+        self.make_widgets()
+
+    def make_widgets(self)->None:
+        # Input field for new watchlist
+        ttk.Label(self, text='New Watchlist:').grid(row=0, column=0)
+        self.watchlist_box = tk.Text(self, width=40, height=10)
+        self.watchlist_box.grid(row=1, column=0,columnspan=3)
+
+        # Load resource (present + added new)
+        ttk.Button(self, text='Load', command=self.load_resource).grid(row=0, column=3)
+    
+    def load_resource(self):
+        #TODO: Load the resource
         pass
+
+root = tk.Tk()
+Watchlist_GUI(parent=root)
+root.mainloop()
