c64ecf60b520b7efdfcf9e1380cf8440c06d30f8
HLIN9 <han.lin1@ucalgary.ca>
2023-05-12 21:33:49 -0600

    adding items done

--- watchlist_gui.py
+++ watchlist_gui.py
@@ -12,6 +12,10 @@ class Watchlist_GUI(ttk.Frame):
         self.watchlist = tk.StringVar()
         self.watchlist.set('Type in new watchlist')
 
+        #TODO: Gets the watchlist type
+        self.watchlist_type = tk.StringVar()
+        self.watchlist_type.set('Anime/Manga')
+
         #TODO: Make the needed widgets
         self.make_widgets()
 
@@ -24,19 +28,54 @@ class Watchlist_GUI(ttk.Frame):
         self.watchlist_box.bind('<Control-Return>', 
                                 self.load_resource)
 
+        self.type = ttk.Entry(self, textvariable=self.watchlist_type)
+        self.type.grid(row=0, column=1)
+
         # Load resource (present + added new)
-        ttk.Button(self, text='Load Watchlist', command=self.load_resource).grid(row=0, column=1)
-        ttk.Button(self, text='Help', command=self.get_help).grid(row=0, column=2)
+        ttk.Button(self, text='Load Watchlist', command=self.load_resource).grid(row=0, column=2)
+        ttk.Button(self, text='Help', command=self.get_help).grid(row=0, column=3)
     
     def load_resource(self, 
-                      event=None)->None:
-        #TODO: # Set the value of self.watchlist to the contents of the Text widget
-        self.watchlist.set(self.watchlist_box.get('1.0', 'end-1c'))
-        messagebox.showinfo('Resource', self.watchlist.get())
-    
+                      event=None) -> None:
+        """
+        This function gets the new watchlist from the Textbox and loads it to the appropriate watchlist table in the database.
+        """
+        # Get the new watchlist
+        new_watchlist = self.watchlist_box.get('1.0', tk.END)
+
+        # Get the watchlist type
+        watchlist_type = self.watchlist_type.get()
+
+        # Connect to the database
+        import sqlite3
+        conn = sqlite3.connect('watchlist.sqlite')
+        c = conn.cursor()
+
+        # Choose the appropriate table based on the watchlist type
+        if watchlist_type.lower() == 'anime':
+            table_name = 'anime_watchlist'
+        elif watchlist_type.lower() == 'manga':
+            table_name = 'manga_watchlist'
+        else:
+            messagebox.showerror('Invalid Watchlist Type', 'Please choose a valid watchlist type (Anime or Manga)')
+            return
+
+        # Get the existing titles in the table
+        existing_titles = [row[0] for row in c.execute(f"SELECT Name FROM {table_name}")]
+        
+        # Add new titles to the table
+        for title in new_watchlist.split('\n'):
+            title = ''.join(c for c in title if c.isalnum()).lower()
+            if title and title not in existing_titles:
+                c.execute(f"INSERT INTO {table_name} (Name, Status) VALUES (?, 'Unread')", (title,))
+        conn.commit()
+        conn.close()
+
+        # Confirm that the new titles were added to the table
+        messagebox.showinfo('Watchlist Updated', 'The new titles were added to the watchlist successfully.')
+
     def get_help(self)->None:
-        #TODO: Show the help messagebox
-        messagebox.showinfo('Help', 'This is a test')
+        pass
 
 root = tk.Tk()
 Watchlist_GUI(parent=root)
